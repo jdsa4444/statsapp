5dab0636838bc2f3e5cfb019914351437a44e9e1
jdsa4444 <johndees@gmail.com>
2017-04-25 14:55:48 -0500

    Add files via upload

---
+++ statsscript.js
@@ -0,0 +1,87 @@
+function HPplus(){
+	//pull in value
+	var HPval = document.getElementById("HP").innerHTML
+	
+	//increment value
+	HPval++;
+	
+	//set new value on page
+	document.getElementById("HP").innerHTML = HPval;
+}
+
+function HPminus(){
+	//pull in value
+	var HPval = document.getElementById("HP").innerHTML
+	
+	//increment value
+	HPval--;
+	
+	//set new value on page
+	document.getElementById("HP").innerHTML = HPval;
+}
+
+function ACplus(){
+	//pull in value
+	var ACval = document.getElementById("AC").innerHTML
+	
+	//increment value
+	ACval++;
+	
+	//set new value on page
+	document.getElementById("AC").innerHTML = ACval;
+}
+
+function ACminus(){
+	//pull in value
+	var ACval = document.getElementById("AC").innerHTML
+	
+	//increment value
+	ACval--;
+	
+	//set new value on page
+	document.getElementById("AC").innerHTML = ACval;
+}
+
+function ATKplus(){
+	//pull in value
+	var ATKval = document.getElementById("ATK").innerHTML
+	
+	//increment value
+	ATKval++;
+	
+	//set new value on page
+	document.getElementById("ATK").innerHTML = ATKval;
+}
+
+function ATKminus(){
+	//pull in value
+	var ATKval = document.getElementById("ATK").innerHTML
+	
+	//increment value
+	ATKval--;
+	
+	//set new value on page
+	document.getElementById("ATK").innerHTML = ATKval;
+}
+
+function SPplus(){
+	//pull in value
+	var SPval = document.getElementById("SP").innerHTML
+	
+	//increment value
+	SPval++;
+	
+	//set new value on page
+	document.getElementById("SP").innerHTML = SPval;
+}
+
+function SPminus(){
+	//pull in value
+	var SPval = document.getElementById("SP").innerHTML
+	
+	//increment value
+	SPval--;
+	
+	//set new value on page
+	document.getElementById("SP").innerHTML = SPval;
+}
